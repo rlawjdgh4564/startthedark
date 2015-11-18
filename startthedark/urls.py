a4602bede858062932ea98db233d22ab9af061f8
kim <rlawjdgh456@naver.com>
2015-11-18 20:11:59 +0900

    lecture 9

--- startthedark/urls.py
+++ startthedark/urls.py
@@ -20,5 +20,6 @@ admin.autodiscover()
 
 urlpatterns = [
 	url(r'^events/', include('events.urls')),
+	url(r'^friends/', include('socialgraph.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
